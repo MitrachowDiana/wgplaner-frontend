750a3e2a0ef32756ab917cc7d3704b4854709437
MitrachowDiana <diana-mitrachow@hotmail.de>
2025-04-24 17:16:48 +0200

    Milestone 2: Vue-Frontend für WG-Planer

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>WG-Planer</h1>
+    <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+
+export default {
+  components: {
+    TaskList
+  }
+}
+</script>
---
+++ src/components/TaskList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Aufgabenliste</h2>
+    <ul v-if="tasks.length > 0">
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.description }} – erledigt: {{ task.done }}
+      </li>
+    </ul>
+    <p v-else>Keine Aufgaben vorhanden</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  mounted() {
+    fetch('http://localhost:8080/todos')
+        .then(res => res.json())
+        .then(data => {
+          this.tasks = data;
+        });
+  }
+};
+</script>
+
