436bbe19f0a200962244c99e014756977f435f2f
Dario-Vujnovic <Dario.Vujnovic@Student.HTW-Berlin.de>
2025-06-05 19:11:39 +0200

    BugFix

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,7 @@
     <!-- Navigation -->
     <Header />
 
-    <!-- TaskList nur auf der Startseite anzeigen -->
-    <section v-if="route.path === '/' && tasks.length > 0" 
-             class="max-w-7xl mx-auto px-4 mt-12 sm:px-6 lg:px-8">
-      <div class="card space-y-6">
-        <h2 class="text-2xl font-semibold text-gray-900">Aktuelle Aufgaben</h2>
-        <TaskList />
-      </div>
-    </section>
+
 
     <!-- Seiteninhalt über Routing -->
     <main class="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
@@ -20,16 +13,5 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRoute } from 'vue-router'
-import Header from './components/Header.vue'
-import TaskList from './components/TaskList.vue'
-import { getTasks } from './api/taskApi'
 
-const route = useRoute()
-const tasks = ref([])
-
-onMounted(async () => {
-  tasks.value = await getTasks()
-})
 </script>
